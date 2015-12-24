08c542ec3c5e984de1f483bc1c8ec40787cc6533
Wernerway <b01901037@ntu.edu.tw>
2015-12-24 20:49:46 +0800

    JSX Parsing & React Loading Succeed

--- webpack-right-index.config.js
+++ webpack-right-index.config.js
@@ -5,6 +5,7 @@ module.exports={
   devtool:'eval',
   entry:'./public/dev/right_index.js',
   output:{
+    path:path.join(__dirname,'public/dev'),
     filename :'bundle.js',
   },
   module:{
@@ -13,17 +14,11 @@ module.exports={
       loader:'babel',
       exclude : /node_modules/,
       include: __dirname,
-      query:{
-        presets:['react']
-      }
     },
     {
       test:/\.jsx$/,
       loader:'babel',
       include:path.join(__dirname,'/public/dev'),
-      query:{
-        presets:['react']
-      }
     }
   ]
   }
